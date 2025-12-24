d3995e8a29f792489d8996a4da7691f05a38a315
Sikorski06 <piotrek3669@wp.pl>
2025-12-24 01:15:21 +0100

    zmiana podejscia w instrukcji nie ma mowy o parametrach w macierzy czyli macierz musi byc zawsze rozwiązywalna

--- podstawienie.c
+++ podstawienie.c
@@ -4,17 +4,28 @@
 
 double *podstawienie(double** matrix, int rows, int columns){
 	double *results = (double *)malloc((columns-1) * sizeof(double));
-	int no_arg;
+	int ile_zer;
+	int indx_row_start;
 	for(int i = rows-1;i >= 0; i--){
-
-   		int ile_zer = 0;
-		for (int j = 0; j < columns-1; j++) {
-   			if(matrix[i][j] == 0)
-				ile_zer += 1;
-		}
-		if(ile_zer == columns-1){
-			if(matrix[i][columns-1] == 0)
-				results[]
-		}
-			
+		ile_zer = 0;
+		for(int j = 0; j < columns-1; j++)
+			if(matrix[i][j] == 0)
+				ile_zer++;
+		if(ile_zer == columns-1 && matrix[i][columns-1] != 0.0){
+			fprintf(stderr, "Blad: układ sprzeczny\n");
+			return NULL;}
+		if(ile_zer != columns-1){
+			indx_row_start = i;
+			break;}
+		if(
 	}
+	int indx_col;
+	for(int i = 0; i< columns-1;i++){
+		if(matrix[indx_row_start][i] != 0.0)
+			indx_col = i;
+	}
+
+	for(int i= indx_col + 1; i < columns-1; i++)
+		results[i] = 0.0;
+	
+}
